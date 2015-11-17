99b81dadab70957767aac077c90d0e8bc9358b81
Todd Hayes <todd@i.tv>
2015-11-17 10:38:31 -0700

    trying stuff

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 
 var app = express();
 
-app.use(express.static(__dirname + '/../public'));
+app.use(express.static(__dirname + '/./public'));
 
 app.listen(8887, function(){
 	console.log("listening on port 8887")
